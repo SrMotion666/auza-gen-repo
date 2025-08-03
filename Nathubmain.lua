a16ee77023c106d819675fe585078fceca98d37b
SrMotion666 <116975268+SrMotion666@users.noreply.github.com>
2025-08-03 16:11:07 +0800

    Update Nathubmain.lua

--- Nathubmain.lua
+++ Nathubmain.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/MSowVF8P/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/Wvc3L3XY/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SrMotion666/Scorpion/refs/heads/main/ScorpionRandomizer.lua", true))()
